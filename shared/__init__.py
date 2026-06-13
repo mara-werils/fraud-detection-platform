3d0491113739a66f2ffc359634d13f864e74d7bd
mara-werils <mefthv447@gmail.com>
2026-06-13 16:40:19 +0500

    refactor(shared): re-export hot reload primitives from package

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,11 +1,15 @@
 """Shared library for the fraud detection platform."""
 
 from shared.config import Settings
+from shared.hot_reload import ConfigReloadError, ConfigSnapshot, HotReloader
 from shared.schemas import AlertEvent, FeatureVector, ScoredTransaction, Transaction
 
 __all__ = [
     "AlertEvent",
+    "ConfigReloadError",
+    "ConfigSnapshot",
     "FeatureVector",
+    "HotReloader",
     "ScoredTransaction",
     "Settings",
     "Transaction",
